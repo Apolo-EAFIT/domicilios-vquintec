5ad3bde95e0306e6d79a58281dd5f7adaeec74da
vquintec <vqc2500@hotmail.com>
2016-06-13 15:38:53 -0500

    ...

--- main.cpp
+++ main.cpp
@@ -27,7 +27,13 @@ void imprimirM(vector<vector<short>> mAdyacencia){
     }
 }
 
+// A C / C++ program for Dijkstra's single source shortest path algorithm.
+// The program is for adjacency matrix representation of the graph
+
+// A utility function to find the vertex with minimum distance value, from
+// the set of vertices not yet included in shortest path tree
 int mindistance(int dist[],int sptset[],int V){
+    // Initialize min value
     int min=INT_MAX,min_index,v;
 
     for(v=0;v<V;v++)
@@ -37,6 +43,7 @@ int mindistance(int dist[],int sptset[],int V){
     return min_index;
 }
 
+// A utility function to print the constructed distance array
 vector<string> print(int dist[],int path[],int dest, int src, int V){
     vector<string> result(4);
     string ruta = to_string(dest) + " <- ";
@@ -59,30 +66,48 @@ vector<string> print(int dist[],int path[],int dest, int src, int V){
 /*
  * Tomado de: http://codepad.org/Jx9hMd3Z
  */
+// Funtion that implements Dijkstra's single source shortest path algorithm
+// for a graph represented using adjacency matrix representation
 vector<string> dijkstra(vector< vector<short> > graph,int src,int dest){
     int V = graph.size();
-    int dist[V],i,count,v;
-    int sptset[V],path[V];
+    int dist[V],i,count,v; // The output array.  dist[i] will hold the shortest
+                           // distance from src to i
+    
+    int sptset[V],path[V]; // sptSet[i] will true if vertex i is included in shortest
+                           // path tree or shortest distance from src to i is finalized
 
+    // Initialize all distances as INFINITE and stpSet[] as false
     for(i=0;i<V;i++)
         dist[i]=INT_MAX, sptset[i]=0;
 
+    // Distance of source vertex from itself is always 0
     dist[src]=0;
     path[src]=0;
 
+    // Find shortest path for all vertices
     for(count=0;count<V-1;count++){
-        int u=mindistance(dist,sptset,V);
+        // Pick the minimum distance vertex from the set of vertices not
+        // yet processed. u is always equal to src in first iteration.
+        int u = mindistance(dist,sptset,V);
+        
         if(u==dest)
             break;
+        
+        // Mark the picked vertex as processed
         sptset[u]=1;
 
-        for(v=0;v<V;v++){
+        // Update dist value of the adjacent vertices of the picked vertex.
+        for(v = 0;v < V;v++){
+            // Update dist[v] only if is not in sptSet, there is an edge from 
+            // u to v, and total weight of path from src to  v through u is 
+            // smaller than current value of dist[v]
             if(!sptset[v]&& graph[u][v] && dist[u]!=INT_MAX && dist[u]+graph[u][v]<dist[v]){
                 dist[v]=dist[u]+graph[u][v];
                 path[v]=u;
             }
         }
     }
+    // Retorna un vector con los datos
     return print(dist,path,dest,src, V);
 }
 
